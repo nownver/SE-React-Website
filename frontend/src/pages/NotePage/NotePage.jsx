d4ce8b90369ec4f6939c87090840e2f7f87c01ee
nownver <urawee.thani@gmail.com>
2023-11-28 23:04:28 +0700

    initialize notepage interface

---
+++ frontend/src/pages/NotePage/NotePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Notes } from "../../components";
+import "./NotePage.css"
+
+const NotePage = () => {
+  return (
+    <div className='notePage'>
+        <Notes />
+    </div>
+  )
+}
+
+export default NotePage;

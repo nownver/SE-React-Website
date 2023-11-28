816f93a6bc62d10a4bb0b347882112fcba0042c7
nownver <urawee.thani@gmail.com>
2023-11-28 22:49:17 +0700

    initialize vital context

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,18 @@
+// import reportWebVitals from '../reportWebVitals';
+
+export { default as ProfilePage } from "./ProfilePage/ProfilePage";
+export { default as RepositoryPage } from "./RepositoryPage/RepositoryPage";
+export { default as LearnPage } from "./LearnPage/LearnPage";
+export { default as LoginPage } from "./LoginPage/LoginPage";
+export { default as RegisterPage } from "./RegisterPage/RegisterPage";
+export { default as HomePage } from "./HomePage/HomePage";
+export { default as EditPage } from "./EditPage/EditPage";
+export { default as FollowerPage } from "./FollowerPage/FollowerPage";
+export { default as FollowingPage } from "./FollowingPage/FollowingPage";
+export { default as UploadPage } from "./UploadPage/UploadPage";
+export { default as NotePage } from "./NotePage/NotePage";
+export { default as TimetablePage } from "./TimetablePage/TimetablePage";
+export { default as ChatPage } from "./ChatPage/ChatPage";
+export { default as NewsPage } from "./NewsPage/NewsPage";
+
+// reportWebVitals();
